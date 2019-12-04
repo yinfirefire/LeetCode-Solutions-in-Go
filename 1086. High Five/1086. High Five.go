9168d304a1cacc412aa284cac980fd23eefcc522
shuoyan <yin.shuo@husky.neu.edu>
2019-12-03 19:28:31 -0800

    update

--- 1086. High Five/1086. High Five.go
+++ 1086. High Five/1086. High Five.go
@@ -1,5 +1,31 @@
 package main
 
-func highFive(items [][]int) [][]int {
+import "sort"
 
+func highFive(items [][]int) [][]int {
+	res := make([][]int, 0)
+	sort.Slice(items, func(a int, b int) bool {
+		if items[a][0] != items[b][0] {
+			return items[a][0] < items[b][0]
+		} else {
+			return items[a][1] >= items[b][1]
+		}
+	})
+	prev := 0
+	sum := 0
+	cnt := 0
+	for _, scores := range items {
+		if scores[0] != prev {
+			cnt = 0
+			prev = scores[0]
+			sum = 0
+		}
+		sum += scores[1]
+		cnt++
+		if cnt == 5 {
+			avg := sum / cnt
+			res = append(res, []int{scores[0], avg})
+		}
+	}
+	return res
 }
--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 --- | --- | ---
 1278 | [Palindrome Partitioning III][1278] | DP
 1218 | [Longest Arithmetic Subsequence of Given Difference][1218] | Hashtable
-
+1086 | [High Five][1086] | Comparator  
 
 
 [1278]: ./1278.%20Palindrome%20Partitioning%20III/
 [1218]: ./1218.%20Longest%20Arithmetic%20Subsequence%20of%20Given%20Difference/
+[1086]: ./1086.%20High%20Five/
