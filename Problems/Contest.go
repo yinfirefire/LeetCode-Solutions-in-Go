c019392c52585d011a13e87a37faa22b8225b710
shuoyan <yin.shuo@husky.neu.edu>
2020-01-12 15:40:56 -0800

    update

--- Problems/Contest.go
+++ Problems/Contest.go
@@ -1,58 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"strconv"
-)
-
-func minimumDistance(word string) int {
-	tb := make(map[int][]int, 26)
-	for i := 0; i < 26; i++ {
-		x := i / 6
-		y := i % 6
-		tb[i] = make([]int, 2)
-		tb[i][0] = x
-		tb[i][1] = y
-	}
-	mem := make(map[string]int)
-	return helper(word, 0, tb, mem, 'a', 'a')
-}
-
-func helper(s string, idx int, tb map[int][]int, mem map[string]int, left, right byte) int {
-	if idx == len(s) {
-		return 0
-	}
-	key := strconv.Itoa(idx) + " " + string(left) + " " + string(right)
-	if v, ok := mem[key]; ok {
-		return v
-	}
-	res := math.MaxInt32
-	c := s[idx]
-	x := tb[int(c-'A')][0]
-	y := tb[int(c-'A')][1]
-	if left == 'a' {
-		res = min(res, helper(s, idx+1, tb, mem, c, right))
-	} else {
-		prevX := tb[int(left-'A')][0]
-		prevY := tb[int(left-'A')][1]
-		dist := abs(prevX-x) + abs(prevY-y)
-		res = min(res, dist+helper(s, idx+1, tb, mem, c, right))
-	}
-	if right == 'a' {
-		res = min(res, helper(s, idx+1, tb, mem, left, c))
-	} else {
-		prevX := tb[int(right-'A')][0]
-		prevY := tb[int(right-'A')][1]
-		dist := abs(prevX-x) + abs(prevY-y)
-		res = min(res, dist+helper(s, idx+1, tb, mem, left, c))
-	}
-	mem[key] = res
-	return res
-}
-
 func main() {
-	fmt.Println(minimumDistance("CAKE"))
 }
 
 func abs(a int) int {
